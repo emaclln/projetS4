95f20cdfbd20a175082d44e8332cf25ca02cd58f
karl9910 <karl.demone@yahoo.com>
2020-05-01 03:00:14 +0200

    essai Ci mais rien ne va

--- projetS4/main.cpp
+++ projetS4/main.cpp
@@ -80,5 +80,76 @@ int main()
 */
 
 
+/*void Graphe::caculCi()
+{
+   std::map<int,std::pair< int,std::vector< std::vector<int>>> > cheminIndice;
+    int depart = 0;
+    int arrive = 5;
+    
+    std::queue<Sommet*> maFile;//file créé
+    maFile.push(m_sommets[depart]);//on note se sommet dans file
 
+    while(!maFile.empty())//tant que file pas null
+    {
+        int *nbre = new int;
+        *nbre = maFile.front()->getId();//on retient ID du sommet
+        maFile.pop();//on libère la file
+        
+        if(*nbre != arrive)
+        {
+            for(auto s : m_sommets[*nbre]->getAdjacent())//pour chaque adjacent
+            {
+                if(s.first->getId() != depart)//si blanc
+                {
+                    maFile.push(s.first);//rajoute à file
+                    
+                    if (cheminIndice.find(s.first->getId()) != cheminIndice.end())
+                    {
+                        if(cheminIndice[s.first->getId()].first > cheminIndice[m_sommets[*nbre]->getId()].first+1)
+                        {
+                            std::pair< int,std::vector< std::vector<int>>> buffer = cheminIndice[m_sommets[*nbre]->getId()];
+                            for(auto i : buffer.second)
+                                i.push_back(m_sommets[*nbre]->getId());
+                            buffer.first += 1;
+                            cheminIndice[s.first->getId()] = buffer;
+                        }
+                        if(cheminIndice[s.first->getId()].first == cheminIndice[m_sommets[*nbre]->getId()].first+1)
+                        {
+                            std::pair< int,std::vector< std::vector<int>>> buffer = cheminIndice[m_sommets[*nbre]->getId()];
+                            std::pair< int,std::vector< std::vector<int>>> buffer1 = cheminIndice[s.first->getId()];
+                            for(auto i : buffer.second)
+                            {
+                                i.push_back(m_sommets[*nbre]->getId());
+                                buffer1.second.push_back(i);
+                            }
+                            cheminIndice[s.first->getId()] = buffer1;
+                        }
+                    }
+                    else
+                    {
+                        if(cheminIndice.find(m_sommets[*nbre]->getId()) != cheminIndice.end())
+                        {
+                            std::pair< int,std::vector< std::vector<int>>> buffer = cheminIndice[m_sommets[*nbre]->getId()];
+                            for(auto i : buffer.second)
+                                i.push_back(m_sommets[*nbre]->getId());
+                            buffer.first += 1;
+                            cheminIndice[s.first->getId()] = buffer;
+                        }
+                        else
+                        {
+                            std::pair< int,std::vector< std::vector<int>>> buffer;
+                            std::vector<int> buffer1;
+                            buffer1.push_back(m_sommets[*nbre]->getId());
+                            buffer.first = 1;
+                            buffer.second.push_back(buffer1);
+                            cheminIndice[s.first->getId()] = buffer;
+                        }
+                    }
+                }
+            }
+        }
+        delete nbre;//libère mémoire
+    }
+
+}*/
 
